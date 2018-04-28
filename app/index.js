d99222ddb6132865544158b5af2e17e811683cd3
RomuloDurante <romulodurante@hotmail.com>
2018-04-28 17:06:40 -0300

    parsing the request and trimed the url

--- app/index.js
+++ app/index.js
@@ -6,11 +6,25 @@
 
 //Dependencies
 const http = require('http');
+const url = require('url');
 
 
 // The server should respond to all request with a string
 const server = http.createServer(function(req, res) {
+
+  //Get the url and Parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  //Get the Path 
+  var path = parsedUrl.pathname;
+  var trimedParse = path.replace(/^\/+|\/+$/g,'');
+
+  //Send the response
   res.end("Hello World!");
+
+  //Log the request path
+  console.log('Request received on path: ' + trimedParse);
+ 
 })
 
 // Start server, and have it listen on port 3000
