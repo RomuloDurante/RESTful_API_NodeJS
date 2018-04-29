d69e1e1754c7cc6fd4320fd1c7522a02af708937
RomuloDurante <romulodurante@hotmail.com>
2018-04-29 08:22:21 -0300

    Parsing Query strings

--- app/index.js
+++ app/index.js
@@ -20,6 +20,10 @@ const server = http.createServer(function(req, res) {
   var path = parsedUrl.pathname;
   var trimedParse = path.replace(/^\/+|\/+$/g,'');  //apara a url
 
+  //Ge the query String as an object
+  var queryStringObject = parsedUrl.query;
+
+
   //Get the HTTP method
   var method = req.method.toLowerCase(); // we need the string to lowercase
 
@@ -27,9 +31,9 @@ const server = http.createServer(function(req, res) {
   res.end("Hello World!");
 
   //Log the request path
-  console.log('Request received on Path->' + trimedParse + ' Method->' + method);
+  console.log('Request received on Path->'+ trimedParse + ' Method->'+ method + 'QueryString->', queryStringObject);
  
 });
 
 // Start server, and have it listen on port 3000
-server.listen(3000, () => console.log("Server starts on port 3000 ..."));
+server.listen(5000, () => console.log("Server starts on port 5000 ..."));
