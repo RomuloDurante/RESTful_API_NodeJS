cb3b93615a732f74b62be951e4910320f1830e2c
RomuloDurante <romulodurante@hotmail.com>
2018-05-01 08:49:07 -0300

    comments

--- app/server/configServer.js
+++ app/server/configServer.js
@@ -1,5 +1,7 @@
 /**
  * Configure the server
+ * 
+ *I know that require + module.exports return a secure object. But I prefer to set my code this way, I *think it gets more organized if i use a IIFE.
  */
 
 ((global)=>{
@@ -20,9 +22,10 @@
 
   //Server Config
   const server = {
-  http: () =>{
-      return  http.createServer( (req, res) =>{ res.end('Hello World !')} ).listen(3000, () => console.log('Server Start PORT-> 3000'));
-  } 
+    //config http server
+      http: () =>{
+          return  http.createServer( (req, res) =>{ res.end('Hello World !')} ).listen(3000, () => console.log('Server Start PORT-> 3000'));
+      }
   }
 
   //expose the server to 'global' object
