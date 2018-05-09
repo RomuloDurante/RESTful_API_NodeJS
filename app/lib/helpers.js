0ab7bc44429b596eeadcd01d53fc4ad2ccf85950
RomuloDurante <romulodurante@hotmail.com>
2018-05-09 10:13:02 -0300

    token generate function

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -73,6 +73,20 @@
       } catch (error) {
         return false;
       }
+   },
+
+   /********************************************************* */
+   // generate TOKEN
+   token: ()=> {
+    var token = '';
+    for (let i = 0; i < 20; i++) {
+      var randon = Math.floor(Math.random() * 25);
+              // ASCII char 
+      token += String.fromCharCode(97 + i) + String.fromCharCode(65 + i) + Math.floor(Math.random() * 10);
+    }
+    
+
+    return token;
    }
 
 
--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 
+var token = '';
+for (let i = 0; i < 20; i++) {
+  var randon = Math.floor(Math.random() * 25);
+ 
+  token += String.fromCharCode(97 + i) + String.fromCharCode(65 + i) + Math.floor(Math.random() * 10);
+}
 
 
 
-var chr = String.fromCharCode(97 + 23)
-console.log(chr);
+
+console.log(token);
