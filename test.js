be7db70ab97192a4f0bb9be97f9a5d340963bb62
RomuloDurante <romulodurante@hotmail.com>
2018-06-23 18:24:37 +0000

    test

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
+var token = '';
 
-// var token = '';
-// for (let i = 0; i < 20; i++) {
-//   var randon = Math.floor(Math.random() * 25);
- 
-//   token += String.fromCharCode(97 + i) + String.fromCharCode(65 + i) + Math.floor(Math.random() * 10);
-// }
+for (let i = 0; i < 20; i++) {
 
+  var randon1 = Math.floor(Math.random() * 25);
+  var randon2 = Math.floor(Math.random() * 25);
+  var randon3 = Math.floor(Math.random() * 10);
 
-var fs = require('fs');
+ // ASCII char 
+ if( i % 2 === 0){
+  token += String.fromCharCode(97 + randon1) + String.fromCharCode(65 + randon2) + randon3;
+ } else {
+  token += String.fromCharCode(65 + randon2) + String.fromCharCode(97 + randon1) + randon3;
+ }
+  
+}
 
-
-var test = fs.readFileSync('./fell1.docx');
-
-console.log(test.toString());
-
+console.log(token);
